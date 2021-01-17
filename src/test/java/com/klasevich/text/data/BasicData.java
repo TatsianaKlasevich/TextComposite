baa4d97941976447d932ed7085da70f1bc4c8aff
TATSIANA KLASEVICH <74313433+TatsianaKlasevich@users.noreply.github.com>
2021-01-17 20:23:40 +0200

    Add files via upload

--- src/test/java/com/klasevich/text/data/BasicData.java
+++ src/test/java/com/klasevich/text/data/BasicData.java
@@ -3,6 +3,7 @@ package com.klasevich.text.data;
 import com.klasevich.text.composite.TextComposite;
 import com.klasevich.text.parser.AbstractParser;
 import com.klasevich.text.parser.ParagraphParser;
+import com.klasevich.text.parser.SentenceParser;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -28,28 +29,30 @@ public class BasicData {
     public static final String PARSED_LEXEME = "It has survived - not only (five) centuries, but also the leap into electronic typesetting, remaining essentially unchanged.";
     public static final String BASIC_CHARACTER = "It has survived";
     public static final String PARSED_CHARACTER = "Ithassurvived";
+    public static final String LONGEST_WORD_TEXT = "It was popularised in the Динамо (Рига) with the release of Letraset sheets.toString() containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker Falcon9 including versions of Lorem Ipsum!";
     public static final List<TextComposite> SORTED_PARAGRAPH;
+    public static final List<TextComposite> SORTED_SENTENCE;
 
     static {
         AbstractParser paragraphParser = new ParagraphParser();
+        AbstractParser sentenceParser = new SentenceParser();
         TextComposite part;
         SORTED_PARAGRAPH = new ArrayList<>();
-        part = paragraphParser.parse("    It is a established fact that a reader will be of a page when looking at its layout...\r\n");
+        SORTED_SENTENCE = new ArrayList<>();
+        part = paragraphParser.parse("It is a established fact that a reader will be of a page when looking at its layout...");
         SORTED_PARAGRAPH.add(part);
-        part = paragraphParser.parse("    Bye бандерлоги.");
+        part = paragraphParser.parse("Bye бандерлоги.");
         SORTED_PARAGRAPH.add(part);
-        part = paragraphParser.parse("    It has survived - not only (five) centuries, but also the leap into electronic\r\n" +
-                "typesetting, remaining essentially unchanged. It was popularised in the “Динамо” (Рига)\r\n" +
-                "with the release of Letraset sheets.toString() containing Lorem Ipsum passages, and\r\n" +
-                "more recently with desktop publishing software like Aldus PageMaker Falcon9 including\r\n" +
-                "versions of Lorem Ipsum!\r\n");
+        part = paragraphParser.parse("It has survived - not only (five) centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the “Динамо” (Рига) with the release of Letraset sheets.toString() containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker Falcon9 including versions of Lorem Ipsum!");
         SORTED_PARAGRAPH.add(part);
-        part = paragraphParser.parse("    It is a long a!=b established fact that a reader will be distracted by the readable\r\n" +
-                "content of a page when looking at its layout. The point of using Ipsum is that it has a\r\n" +
-                "more-or-less normal distribution ob.toString(a?b:c), as opposed to using (Content here),\r\n" +
-                "content here's, making it look like readable English?\r\n");
+        part = paragraphParser.parse("It is a long a!=b established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Ipsum is that it has a more-or-less normal distribution ob.toString(a?b:c), as opposed to using (Content here), content here's, making it look like readable English?");
         SORTED_PARAGRAPH.add(part);
-        System.out.println(SORTED_PARAGRAPH);
+        part = sentenceParser.parse("It was popularised in the Динамо (Рига) with the release of Letraset sheets.toString() containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker Falcon9 including versions of Lorem Ipsum!");
+        SORTED_SENTENCE.add(part);
+        part = sentenceParser.parse("The point of using Ipsum is that it has a more-or-less normal distribution ob.toString(a?b:c), as opposed to using (Content here), content here's, making it look like readable English?");
+        SORTED_SENTENCE.add(part);
+        part = sentenceParser.parse("It is a long a!=b established fact that a reader will be distracted by the readable content of a page when looking at its layout.");
+        SORTED_SENTENCE.add(part);
     }
 
     private BasicData() {
